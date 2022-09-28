aee2581f15732a7839332c814d9177f0d817c355
sangoamon <114050862+sangoamon@users.noreply.github.com>
2022-09-28 08:16:44 +0300

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -1,13 +1,23 @@
-dante_turn = input('Choose Dante action: ')
-vergil_turn = input('Choose Vergil action: ')
+import random
+actions = ['Rock', 'Paper', 'Scissors']
 
-if dante_turn == vergil_turn:
-    print('They are both injured and cant continue the fight')
-elif dante_turn == 'Scissors' and vergil_turn == 'Rock':
-    print('Dante defeated Vergil')
-elif dante_turn == 'Rock' and vergil_turn == 'Scissors':
-    print('Dante defeated Vergil')
-elif dante_turn == 'Paper' and vergil_turn == 'Rock':
-    print('Dante defeated Vergil')
-else:
-    print('Saul Goodman 3D')
+
+while(True):
+    dante_turn = input('Choose Dante action, or type exit: ')
+    vergil_turn = random.choice(actions)
+
+    if dante_turn == 'exit':
+        print('Thank you for ... something')
+        break
+
+    print(f'Dante:{dante_turn} versus Vergil:{vergil_turn}')
+    if dante_turn == vergil_turn:
+        print('They are both injured and cant continue the fight')
+    elif dante_turn == 'Scissors' and vergil_turn == 'Rock':
+        print('Dante defeated Vergil')
+    elif dante_turn == 'Rock' and vergil_turn == 'Scissors':
+        print('Dante defeated Vergil')
+    elif dante_turn == 'Paper' and vergil_turn == 'Rock':
+        print('Dante defeated Vergil')
+    else:
+        print('Saul Goodman 3D')
